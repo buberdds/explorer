ce0c8761d90419377e35b2e55a6fdd1701c499dc
Michal Zielenkiewicz <michal@oasisprotocol.org>
2023-04-20 13:53:53 +0200

    Gitlint check init

---
+++ internals/scripts/gitlint.js
@@ -0,0 +1,15 @@
+// @ts-check
+const execSync = require('child_process').execSync
+
+const GIT_ORIGIN_REMOTE = 'origin'
+const RELEASE_BRANCH = 'master'
+const BRANCH = `${GIT_ORIGIN_REMOTE}/${RELEASE_BRANCH}`
+const COMMIT_SHA = require('child_process').execSync(`git rev-parse ${BRANCH}`).toString().trim()
+
+console.log(`*** Running gitlint for commits from ${BRANCH} (${COMMIT_SHA})`)
+
+try {
+  execSync(`gitlint --commits ${BRANCH}..HEAD`, { stdio: 'inherit' })
+} catch (error) {
+  process.exit(1)
+}
